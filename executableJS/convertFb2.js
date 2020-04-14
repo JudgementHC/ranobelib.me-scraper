66eb50e1408a9a822858e4e80918dbacfbccb420
nurs-dev <nurgaliev_nursultan@mail.ru>
2020-04-14 11:08:22 +0600

    promises are added

--- executableJS/convertFb2.js
+++ executableJS/convertFb2.js
@@ -7,20 +7,23 @@ const formData = {
 	target_format: 'fb2',
 	source_file: fs.createReadStream('./ranobe/World of otome game is tough for mobs(vol.4, chap.123-165).epub')
 }; */
-const sendRanobe = (formData, apiKey) => {
-	request.post(
-		{
-			url: 'https://sandbox.zamzar.com/v1/jobs/',
-			formData,
-		}, 
-		function (err, response, body) {
-			if (err) {
-				console.error('Unable to start conversion job', err);
-			} else {
-				console.log('SUCCESS! Conversion job started:', JSON.parse(body));
+const sendRanobe = async (formData, apiKey) => {
+	await new Promise(resolve => {
+		request.post({
+				url: 'https://sandbox.zamzar.com/v1/jobs/',
+				formData,
+			},
+			function (err, response, body) {
+				if (err) {
+					console.error('Unable to start conversion job', err);
+				} else {
+					console.log('SUCCESS! Conversion job started:', JSON.parse(body));
+				}
 			}
-		}
-	).auth(apiKey, '', true);
+		).auth(apiKey, '', true);
+
+		resolve();
+	})
 }
 
 
@@ -28,47 +31,53 @@ const sendRanobe = (formData, apiKey) => {
 	apiKey = 'f836cd51aacaa3ddc97156339f8381e6cca888be',
 	jobID = 15; */
 
-const checkRequest = (jobID, apiKey) => {
-	request.get(
-		`https://sandbox.zamzar.com/v1/jobs/${jobID}`,
-		function (err, response, body) {
-			if (err) {
-				console.error('Unable to get job', err);
-			} else {
-				console.log('SUCCESS! Got job:', JSON.parse(body));
-			}
-		}
-	).auth(apiKey, '', true);
-}
-
-
-const getRanobe = (fileID, fileName, apiKey) => {
-	request.get(
-		{
-			url: 'https://sandbox.zamzar.com/v1/files/' + fileID + '/content',
-			followRedirect: false
-		},
-		function (err, response, body) {
-			if (err) {
-				console.error('Unable to download file:', err);
-			} else {
-				// We are being redirected
-				if (response.headers.location) {
-					// Issue a second request to download the file
-					var fileRequest = request(response.headers.location);
-					fileRequest.on('response', function (res) {
-						res.pipe(fs.createWriteStream(`./ranobe/${fileName}.fb2`));
-					});
-					fileRequest.on('end', function () {
-						console.log('File download complete');
-					});
+const checkRequest = async (jobID, apiKey) => {
+	await new Promise(resolve => {
+		request.get(
+			`https://sandbox.zamzar.com/v1/jobs/${jobID}`,
+			function (err, response, body) {
+				if (err) {
+					console.error('Unable to get job', err);
+				} else {
+					return JSON.parse(body)
 				}
 			}
-		}
-	).auth(apiKey, '', true).pipe(fs.createWriteStream(`./ranobe/${fileName}.fb2`));
+		).auth(apiKey, '', true);
+
+		resolve();
+	})
+}
+
+
+const getRanobe = async (fileID, fileName, apiKey) => {
+	await new Promise(resolve => {
+		request.get({
+				url: 'https://sandbox.zamzar.com/v1/files/' + fileID + '/content',
+				followRedirect: false
+			},
+			function (err, response, body) {
+				if (err) {
+					console.error('Unable to download file:', err);
+				} else {
+					// We are being redirected
+					if (response.headers.location) {
+						// Issue a second request to download the file
+						var fileRequest = request(response.headers.location);
+						fileRequest.on('response', function (res) {
+							res.pipe(fs.createWriteStream(`./ranobe/${fileName}.fb2`));
+						});
+						fileRequest.on('end', function () {
+							console.log('File download complete');
+						});
+					}
+				}
+			}
+		).auth(apiKey, '', true).pipe(fs.createWriteStream(`./ranobe/${fileName}.fb2`));
+
+		resolve();
+	})
 }
 
 
 
-// export default {sendRanobe, checkRequest, getRanobe}
 exports.convertFb2 = { sendRanobe, getRanobe, checkRequest };
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /* DEPENDENCIES */
 const Epub = require("epub-gen");
 const prompt = require('prompt');
+const fs = require('fs');
 
 /* LOCAL FILE IMPORT */
 const { convertFb2 } = require('./executableJS/convertFb2')
@@ -34,6 +35,18 @@ const ranobeSchema = {
 			description: 'Name of title',
 			message: 'Required parameter',
 			required: true
+		},
+		convertToFb2: {
+			description: 'Do you need to convert epub to fb2? yes/no'
+		},
+		convertApiKey: {
+			description: 'Input api key from https://developers.zamzar.com/user',
+			ask: () => {
+				const historyValue = prompt.history('convertToFb2').value
+				return historyValue === 'yes' || historyValue === 'y'
+			},
+			required: true,
+			message: 'Required parameter'
 		}
 	}
 };
@@ -50,5 +63,14 @@ prompt.get(ranobeSchema, async function (err, result) {
 		content: options
 	};
 
-	new Epub(option, `./ranobe/${titleName}(vol.${ver}, chap.${firstChapter}-${lastChapter}).epub`);
+	await new Epub(option, `./ranobe/${titleName}(vol.${ver}, chap.${firstChapter}-${lastChapter}).epub`);
+
+	/* const apiKey = 'f836cd51aacaa3ddc97156339f8381e6cca888be'
+	const formData = {
+		target_format: 'fb2',
+		source_file: fs.createReadStream('./ranobe/World of otome game is tough for mobs(vol.4, chap.123-165).epub')
+	}; */
+	
+	await convertFb2.sendRanobe();
+	
 });
